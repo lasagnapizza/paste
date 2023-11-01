fd66a5817372d636f97754ecefc95de8ff16fc53
kinduff <kinduff@protonmail.com>
2023-11-01 22:04:04 +0100

    Updates README

--- README.md
+++ README.md
@@ -1,24 +1,7 @@
-# README
+# paste
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+**paste** is a **simple markdown store**, like pastebin but it **renders as markdown**, a few things to have in mind:
 
-Things you may want to cover:
+## Contributing
 
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ body = %{# paste
 
 **paste** is a **simple markdown store**, like pastebin but it **renders as markdown**, a few things to have in mind:
 
-- you can [create a paste](/), which is text in Markdown format, and it will be saved to a URL
-- you can specify an edit password; otherwise, the system will provide you with one in order to edit the paste in the future
+- you can [create a note](/), which is text in Markdown format, and it will be saved to a URL
+- you can specify an edit password; otherwise, the system will provide you with one in order to edit the note in the future
 - if you forget the edit password, there is nothing you can do, just create a new note
 - you can specify the URL slug; if not provided, the system will generate one for you
 - content hosting is free, but it may be removed without notice in case of any infringement
