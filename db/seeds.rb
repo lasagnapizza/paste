c6e861afbb8a0fb91743a6ca374938e0067505cf
kinduff <kinduff@protonmail.com>
2023-11-01 17:18:04 +0100

    Adds about page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+body = %{
+# paste
+
+paste is a simple markdown store, image pastebin but for markdown files, a few things to have in mind:
+
+- you can create a paste, which is text in Markdown format, and it will be saved to a URL
+- you can specify an edit password; otherwise, the system will provide you with one in order to edit the paste in the future
+- if you forget the edit password, there is nothing you can do, just create a new note
+- you can specify the URL slug; if not provided, the system will generate one for you
+- content hosting is free, but it may be removed without notice in case of any infringement
+
+paste supports most markdown features (GFM) and code highlight for multiple languages, just use regular markdown
+
+send a message to contact@lasagna.pizza in case you need something (no password reset requests)
+}
+Note.create(body:, slug: "about", password: Note.random_password)
