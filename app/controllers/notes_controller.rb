61e10e8ba3d86ca75c4b360da9b27271a52b371a
kinduff <kinduff@protonmail.com>
2023-11-16 14:08:42 +0100

    Improved the notes controller.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,16 +1,15 @@
 class NotesController < ApplicationController
+  before_action :set_note, only: [:edit, :show, :update]
+
   def new
     @note = Note.new
   end
 
   def edit
-    @note = Note.find_by!(slug: params[:id])
   end
 
   def show
     @noindex = true
-
-    @note = Note.find_by!(slug: params[:id])
     Note.increment_counter(:views, @note)
   end
 
@@ -26,21 +25,18 @@ class NotesController < ApplicationController
   end
 
   def update
-    @note = Note.find_by!(slug: params[:id])
-
     unless @note.authenticate(note_params[:password]) # xkcd 2347
-      flash[:error] = "wrong password"
+      flash.now[:error] = "wrong password"
       render :edit, status: :unprocessable_entity
       return
     end
 
-    @note.assign_attributes(note_params.slice(:body, :slug))
     @note.password = note_params[:new_password] if note_params[:new_password].present?
 
     msg = "note updated"
     msg += ", the edit password is #{@note.password}" if @note.password
 
-    if @note.save
+    if @note.update(note_params.slice(:body, :slug))
       redirect_to note_path(@note.slug), flash: {info: msg}
     else
       render :edit, status: :unprocessable_entity
@@ -51,5 +47,9 @@ class NotesController < ApplicationController
 
   def note_params
     params.require(:note).permit(:body, :password, :new_password, :slug)
+  end
+
+  def set_note
+    @note = Note.find_by!(slug: params[:id])
   end
 end
